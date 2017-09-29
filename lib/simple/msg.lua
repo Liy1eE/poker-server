87e6d76170fd20fe25f5195bd72341e9cceb3bda
liuyang <stb@qq.com>
2017-09-29 17:51:18 +0800

    字幕排序

--- lib/simple/msg.lua
+++ lib/simple/msg.lua
@@ -12,55 +12,41 @@ of this license document, but changing it is not allowed.
 --]]
 
 return {
-    DEBUG = 0x0001,
-    TICK = 0x0002,
-    NEWS = 0x0003,
-    DISCONNECT = 0x0004,
-    KICK = 0x0009,
-    LOGIN = 0x0010,
-    
-    ------LOBBY---------
-    COIN = 0x0023,
-    CASH = 0x01010,
-    REFRESH_PAY = 0x1013,
-    
-    INVITER = 0x0036,
-    INVITER_REWARD = 0x0037,
-    
-    IDENTIFY = 0x1014,
-    
     ACCREDIT_COUNT = 0x1015,
+    AGREE = 0x0028,
+    APPLY = 0x0027,
     BE_ACCREDIT = 0x1016,
     CANCEL_ACCREDIT = 0x1017,
-    
-    ------ROOM----------
+    CASH = 0x01010,
+    COIN = 0x0023,
     CREATE = 0x0011,
-    ENTER = 0x0012,
-    
-    START_GAME = 0x001A,
-    MID_ENTER = 0x001B,
-    VISITOR = 0x001F,
-    ------------------
-    RENTER = 0x0013,
-    READY = 0x0014,
-    GET_ROOM = 0x1018,
-    INIT = 0x0024,
-    RESULT = 0x0025,
-    
-    
-    APPLY = 0x0027,
-    AGREE = 0x0028,
-    GPS = 0x0029,
-    
-    SMILE = 0x0031,
+    DEBUG = 0x0001,
+    DISCONNECT = 0x0004,
     DISMISS = 0x0032,
-    ROOM_OUT = 0x0033,
-    PAUSE = 0x0034,
-    SEND_MSG = 0x0038,
+    ENTER = 0x0012,
     GET_MSG_LIST = 0x0039,
-    
+    GET_ROOM = 0x1018,
+    GPS = 0x0029,
+    IDENTIFY = 0x1014,
+    INIT = 0x0024,
+    INVITER = 0x0036,
+    INVITER_REWARD = 0x0037,
+    KICK = 0x0009,
+    LOGIN = 0x0010,
+    MID_ENTER = 0x001B,
+    NEWS = 0x0003,
     OFFLINE = 0x1008,
-    
-    UPLOAD_VOICE = 0x1011,
+    PAUSE = 0x0034,
     PLAY_VOICE = 0x1012,
+    READY = 0x0014,
+    REFRESH_PAY = 0x1013,
+    RENTER = 0x0013,
+    RESULT = 0x0025,
+    ROOM_OUT = 0x0033,
+    SEND_MSG = 0x0038,
+    SMILE = 0x0031,
+    START_GAME = 0x001A,
+    TICK = 0x0002,
+    UPLOAD_VOICE = 0x1011,
+    VISITOR = 0x001F,
 }
